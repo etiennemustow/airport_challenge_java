d85ef569450d87c42f7a51750907833205de14be
etiennemustow <etienne.o.mustow@gmail.com>
2017-09-28 18:28:52 +0100

    Everything, bar one error: takeoff

--- src/Plane.js
+++ src/Plane.js
@@ -1,7 +1,16 @@
 'use strict'
 
-function Plane(){}
+function Plane(){
+
+}
 
 Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
+  this._location = airport
 };
+
+Plane.prototype.takeoff = function(){
+  this._location.clearForTakeOff();
+
+
+}
